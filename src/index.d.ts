0fa177715c3ed764e20752a8a9a20739f61c6e99
Meir Ohayon <35038718+meir-ohayon@users.noreply.github.com>
2018-01-26 12:02:07 +0200

    Create index.d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+import { ModuleWithProviders } from '@angular/core';
+export { DataService } from './data.service.js';
+
+export declare class AngularDataset { 
+	static forRoot(): ModuleWithProviders;    
+}
